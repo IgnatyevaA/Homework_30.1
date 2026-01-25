5064a27dd4b6890a38996482c353673b9cc26c27
IgnatyevaA <shyra9072@gmail.com>
2026-01-25 11:35:05 +0300

    fourth commit  Changes to be committed: 	modified:   users/serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -25,7 +25,10 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     
     def create(self, validated_data):
         validated_data.pop('password_confirm')
-        user = User.objects.create_user(**validated_data)
+        password = validated_data.pop('password')
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
         return user
 
 
